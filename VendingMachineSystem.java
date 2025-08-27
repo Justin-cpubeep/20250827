e63527cb6579128b71cce81d63c45f0bc1bf46e4
seungmoson <seungmo975@gmail.com>
2025-08-27 17:27:00 +0900

    라면, 음료 연동완료

--- VendingMachineSystem.java
+++ VendingMachineSystem.java
@@ -6,12 +6,13 @@ public class VendingMachineSystem {
         CoffeeM coffeeMachine = new CoffeeM();
         // 라면 자판기 객체 생성
         RamenM ramenMachine = new RamenM();
+        // VM2_BeverageMachine 클래스는 static 메소드를 사용하므로 객체 생성이 필요 없습니다.
 
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
             System.out.println("\n[전체 시스템] 사용할 자판기를 선택하세요.");
-            System.out.println("[1] 라면자판기  [2] 시스템 종료 ");
+            System.out.println("[1] 라면 자판기  [2] 음료 자판기  [3] 시스템 종료");
             System.out.print("선택: ");
             String choice = scanner.next();
 
@@ -20,6 +21,9 @@ public class VendingMachineSystem {
                     ramenMachine.runVendingMachine(); // 라면 자판기 메뉴 실행
                     break;
                 case "2":
+                    VM2_BeverageMachine.run(scanner); // 음료 자판기 메뉴 실행
+                    break;
+                case "3":
                     System.out.println("시스템을 종료합니다.");
                     scanner.close();
                     return;
